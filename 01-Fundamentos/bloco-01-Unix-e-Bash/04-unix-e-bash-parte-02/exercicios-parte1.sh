6bcb397f02438ae25efeb3307d8284ec348fcc2f
wanderson-rpf <wandersonrpf@gmail.com>
2022-07-09 17:03:13 -0300

    Adicionando respostas para os exercicios B1 D4 P1

---
+++ 01-Fundamentos/bloco-01-Unix-e-Bash/04-unix-e-bash-parte-02/exercicios-parte1.sh
@@ -0,0 +1,56 @@
+delay=1
+
+#Exercicio 1
+echo "-> Acessando pasta unix_tests"
+cd unix_tests
+sleep $delay
+
+echo "-> Criar arquivo skills2.txt e adicioando valores"
+echo "Internet\nUnix\nBash" > skills2.txt
+sleep $delay
+
+#Exercicio 3
+echo "-> Adicionando 5 itens a lista"
+echo "HTML\nCSS\nJavaScript\nLinux\nAlgoritmo" >> skills2.txt
+sleep $delay
+
+#Exercicio 4
+echo "-> Quandidade de linhas arquivo skills2.txt"
+wc -l skills2.txt
+sleep $delay
+
+#Exercicio 5
+echo "-> Criando arquivo top_skills.txt usando o skills2.txt"
+cat < skills2.txt > top_skills.txt
+sleep $delay
+
+#Exercicio 6
+echo "-> Criando arquivo phrases2.txt e adicionando frases"
+echo "Teste de arquivo.\nNovas frases para teste.\nArquivo para teste em phrases2." > phrases2.txt
+sleep $delay
+
+#Exercicio 7
+echo "-> Numero de linhas que contem as letras br"
+grep -c "br" phrases2.txt
+sleep $delay
+
+#Exercicio 8
+echo "-> Numero de linhas que NAO contem as letras br"
+grep -v "br" phrases2.txt
+sleep $delay
+
+#Exercicio 9
+echo "-> Adicionando o nome de dois paises ao arquivo phrases2.txt"
+echo "Brasil\nArgentina" >> phrases2.txt
+sleep $delay
+
+Exercicio 10
+echo "-> Criando novo arquivo bunch_of_things.txt e adicionando dados de phrases2.txt e countries.txt"
+cat < phrases2.txt  >> bunch_of_things.txt
+cat < countries.txt >> bunch_of_things.txt
+sleep $delay
+
+#Exercicio 11
+echo "-> Ordenando arquivo bunch_of_things.txt"
+sort bunch_of_things.txt
+sleep $delay
