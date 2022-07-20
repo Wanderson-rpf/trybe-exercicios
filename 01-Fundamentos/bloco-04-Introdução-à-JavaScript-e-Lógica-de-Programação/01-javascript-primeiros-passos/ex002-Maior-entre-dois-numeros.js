99a20938931122dcbd83afc8044b5182c7c98792
wanderson-rpf <wandersonrpf@gmail.com>
2022-07-20 16:56:56 -0300

    Adicionando exercicio 001 e 002

---
+++ 01-Fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/01-javascript-primeiros-passos/ex002-Maior-entre-dois-numeros.js
@@ -0,0 +1,10 @@
+console.log('-->Execicio 2:');
+// 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+const num1 = 10;
+const num2 = 6;
+
+if(num1>num2){
+  console.log(`O maior número é ${num1}`)
+}else{
+  console.log(`O maior número é ${num2}`)
+}
