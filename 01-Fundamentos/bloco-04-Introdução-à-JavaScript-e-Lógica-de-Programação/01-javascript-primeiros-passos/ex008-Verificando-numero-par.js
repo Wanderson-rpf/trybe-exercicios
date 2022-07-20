a6bd80bddd882b05274886f184b94d0101e37488
wanderson-rpf <wandersonrpf@gmail.com>
2022-07-20 19:06:45 -0300

    Adicionando exercicios 007, 008 e 009

---
+++ 01-Fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/01-javascript-primeiros-passos/ex008-Verificando-numero-par.js
@@ -0,0 +1,11 @@
+// 8. 🚀 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+// Bonus: use somente um if.
+
+const n1 = 2;
+const n2 = 3;
+const n3 = 4;
+
+if(n1%2===0 || n2%2===0 || n3%2===0){
+  console.log(true)
+}else{console.log(false)}
+
