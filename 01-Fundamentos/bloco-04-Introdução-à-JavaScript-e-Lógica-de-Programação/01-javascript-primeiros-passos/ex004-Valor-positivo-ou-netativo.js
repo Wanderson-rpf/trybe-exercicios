f1999c285ac05325a263ffd7924ef966ee6782ec
wanderson-rpf <wandersonrpf@gmail.com>
2022-07-20 17:01:23 -0300

    Adicionando exercicio 003 e 004

---
+++ 01-Fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/01-javascript-primeiros-passos/ex004-Valor-positivo-ou-netativo.js
@@ -0,0 +1,10 @@
+console.log('-->Execicio 4:');
+// 4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const valor = -5;
+
+if(valor>=0){
+  console.log('Positive')
+}else{
+  console.log('Negative')
+}
