600c249bfb08b62bf5896b6ba59249dcdad9d518
wanderson-rpf <wandersonrpf@gmail.com>
2022-07-21 12:50:05 -0300

    Adicionando exercicios 007, 008 e 009

---
+++ 01-Fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/02-javascript-array-e-loop-for/ex008-Criando-um-array-de-1-ate-25.js
@@ -0,0 +1,6 @@
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+let array =[];
+for(let i=1;i<=25;i++){
+  array.push(i)
+}
+console.log(`Array de 1 até 25 criado: ${array}`);
