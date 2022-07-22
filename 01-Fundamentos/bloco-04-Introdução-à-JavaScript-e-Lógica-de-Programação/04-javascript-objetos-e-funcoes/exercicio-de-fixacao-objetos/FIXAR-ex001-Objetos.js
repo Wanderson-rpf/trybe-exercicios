a7dc609d466b488e08a57c66744880330342fed2
wanderson-rpf <wandersonrpf@gmail.com>
2022-07-22 14:29:53 -0300

    Adicionando exercicio de fixação de objetos.

--- 01-Fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/04-javascript-objetos-e-funcoes/exercicio-de-fixacao-objetos/FIXAR-ex001-Objetos.js
+++ 01-Fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/04-javascript-objetos-e-funcoes/exercicio-de-fixacao-objetos/FIXAR-ex001-Objetos.js
@@ -3,7 +3,7 @@
 // let lastName = 'Silva';
 // let age = 34;
 // let medals = { golden: 2, silver: 3 };
-console.log(`\nExecircio 1: Criando objeto.`)
+console.log(`\nExercício 1: Criando objeto.`)
 let pessoa = {
   name: 'Marta',
   lastname: 'Silva',
@@ -16,23 +16,23 @@ let pessoa = {
 
 // 2 - Acesse as chaves name, lastName e age e concatene as suas informações 
 // para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
-console.log(`\nExecircio 2: Consultando informção do objeto.`)
+console.log(`\nExercício 2: Consultando informção do objeto.`)
 console.log(`A jogadora ${pessoa.name} ${pessoa.lastname} tem ${pessoa.age} anos de idade.`);
 
 // 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as 
 // datas em que a jogadora Marta foi considerada a melhor do mundo.
 // [2006, 2007, 2008, 2009, 2010, 2018]
-console.log(`\nExecircio 3: Adicionando valores ao objeto.`)
+console.log(`\nExercício 3: Adicionando valores ao objeto.`)
 pessoa['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 console.table(pessoa);
 
 // 4 - Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: 
 // "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
-console.log(`\nExecircio 4: Consultando elementos do objeto adicionado.`)
+console.log(`\nExercício 4: Consultando elementos do objeto adicionado.`)
 console.log(`A jogadora ${pessoa.name} ${pessoa.lastname} foi eleita a melhor do mundo por ${pessoa.bestInTheWorld.length} vezes.`)
 
 // 5 - Acesse a chave medals e faça um console.log no seguinte formato: 
 // "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
-console.log(`\nExecircio 5: Consultando elementos do objeto adicionado.`)
+console.log(`\nExercício 5: Consultando elementos do objeto adicionado.`)
 console.log(`A jogadora possui ${pessoa.medals.golden} medalhas de ouro e ${pessoa.medals.silver} medalhas de prata.`)
 
