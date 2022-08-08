abb0e7a4216624922ab78b808144f7d9134fbb50
wanderson-rpf <wandersonrpf@gmail.com>
2022-08-08 14:23:18 -0300

    Atualizando exercicio de LocalStorage

--- 01-Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/04-javascript-web-storage/assets/js/main.js
+++ 01-Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/04-javascript-web-storage/assets/js/main.js
@@ -1,14 +1,12 @@
-const backgrounds = ['rgb(22, 56, 76)', 'rgb(9, 22, 33)', 'rgb(47, 54, 62)', 'rgb(255, 218, 64)', 'rgb(255, 255, 255)'];
-const colorLetter = ['rgb(255, 218, 64)', 'rgb(255, 255, 255)', 'rgb(127, 255, 0)', 'rgb(40, 123, 255)', 'rgb(8, 74, 35)'];
-
-const textArea = document.querySelector('.text');
-console.log(textArea);
-
 const backgroundPainel = document.querySelector('#background');
 const colorLetterPainel = document.querySelector('#color-letter');
 const fontSizePainel = document.querySelector('#font-size');
 const fontTypePainel = document.querySelector('#font-type');
 const defaultPainel = document.querySelector('#default');
+const textArea = document.querySelector('.text');
+const backgrounds = ['rgb(22, 56, 76)', 'rgb(9, 22, 33)', 'rgb(47, 54, 62)', 'rgb(255, 218, 64)', 'rgb(255, 255, 255)'];
+const colorLetter = ['rgb(255, 218, 64)', 'rgb(255, 255, 255)', 'rgb(127, 255, 0)', 'rgb(40, 123, 255)', 'rgb(8, 74, 35)'];
+const fontSize = ['0.8rem','1rem', '1.3rem', '1,5rem', '2rem'];
 
 defaultPainel.addEventListener('click', function() {
   textArea.style.backgroundColor = '#fff8dc';
@@ -44,5 +42,21 @@ colorLetterPainel.addEventListener('click', function() {
     textArea.style.color = colorLetter[4];
   }else if(textArea.style.color === colorLetter[4]) {
     textArea.style.color = colorLetter[0];
+  }
+});
+
+fontSizePainel.addEventListener('click', () => {
+  if(textArea.style.fontSize === '1rem' || textArea.style.fontSize === '') {
+    textArea.style.fontSize = fontSize[0];
+  } else if(textArea.style.fontSize === fontSize[0]) {
+    textArea.style.fontSize = fontSize[1];
+  } else if(textArea.style.fontSize === fontSize[1]) {
+    textArea.style.fontSize = fontSize[2];
+  } else if(textArea.style.fontSize === fontSize[2]) {
+    textArea.style.fontSize = fontSize[3];
+  } else if(textArea.style.fontSize === fontSize[3]) {
+    textArea.style.fontSize = fontSize[4];
+  }else if(textArea.style.fontSize === fontSize[4]) {
+    textArea.style.fontSize = fontSize[0];
   }
 });
