8588ba9663de934240e78e29880736745e31630f
wanderson-rpf <wandersonrpf@gmail.com>
2022-08-23 20:01:17 -0300

    :rocket: Concluído atividades bloco 8 dia 3

--- 01-Fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/03-JavaScript-ES6-Higher-Order-Functions-map-e-filter/ex001.js
+++ 01-Fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/03-JavaScript-ES6-Higher-Order-Functions-map-e-filter/ex001.js
@@ -78,5 +78,62 @@ console.log(formatedBookNames())
 // o livro foi lançado.
 console.log('\n<------------------- exercicio 02 ------------------->');
 function nameAndAge() {
-  
+  const listAuthors = [];
+  books.map((element) => {
+    listAuthors.push({
+      age: 2022 - element.author.birthYear,
+      author: element.author.name,
+    });
+  })
+  listAuthors.sort((a, b) => a.age - b.age);
+  return listAuthors;
+}
+
+console.log(nameAndAge());
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+console.log('\n<------------------- exercicio 03 ------------------->');
+function fantasyOrScienceFiction() {
+  return books.filter((item) => item.genre === 'Ficção Científica' || item.genre === 'Fantasia');
+}
+console.log(fantasyOrScienceFiction());
+
+// 4 - Crie um array formado pelos livros com mais de 60 anos desde sua publicação. 
+// Esse array deve ser ordenado do livro mais velho ao mais novo.
+console.log('\n<------------------- exercicio 04 ------------------->');
+function oldBooksOrdered() {
+  return books.filter((item) => (2022 - item.releaseYear) >= 60).sort((a, b) => a.releaseYear - b.releaseYear);
+}
+console.log(oldBooksOrdered());
+
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+console.log('\n<------------------- exercicio 05 ------------------->');
+function fantasyOrScienceFictionAuthors() {
+  return books.filter((item) => item.genre === 'Fantasia' || item.genre === 'Ficção Científica')
+  .map((item) => item.author.name).sort();
+}
+console.log(fantasyOrScienceFictionAuthors())
+
+// 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+console.log('\n<------------------- exercicio 06 ------------------->');
+function oldBooks() {
+  return books.filter((item) => (2022 - item.releaseYear) >= 60).map((item) => item.name);
+}
+console.log(oldBooks());
+
+// 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+console.log('\n<------------------- exercicio 07 ------------------->');
+// * feito com ajuda do gabarito.
+function authorWith3DotsOnName() {
+  return books.find((item) => item.author.name.split(' ').filter((item) => item.endsWith('.')).length === 3).name;
+}
+console.log(authorWith3DotsOnName());
+
+// outra forma de fazer
+function authorWith3DotsOnName() {
+  return books.filter((book) => (
+    book.author.name[1] === '.'
+    && book.author.name[4] === '.'
+    && book.author.name[7] === '.'
+  ))[0].name;
 }
