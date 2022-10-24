f4b6573fc1b182bff3a8b7a7b3da72240c97869e
wanderson-rpf <wandersonrpf@gmail.com>
2022-10-24 20:55:58 -0300

    :construction: [progress] Instalando e importando recursos iniciais createStore e DevTools

--- 02-Desenvolvimento-Front-end/Seção-07-Gerenciamento-de-estado-com-Redux/01-Introdução-ao-Redux-O-estado-global-da-aplicação/ex001/script.js
+++ 02-Desenvolvimento-Front-end/Seção-07-Gerenciamento-de-estado-com-Redux/01-Introdução-ao-Redux-O-estado-global-da-aplicação/ex001/script.js
@@ -1,4 +1,17 @@
+// Importando o REDUX fora da versão RTK
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
 const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+
+// 1 - Crie uma store para a nossa aplicação.
+const store = createStore(reducer);
+
+// 2 - Crie um reducer, implementando um switch que retorne apenas o estado inicial como default.
+const reducer = (state = INITIAL_STATE, action) => {
+  return state
+}
+
