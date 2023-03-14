4dfeebb9ba70fe476d7687bb4234f1c743a18d1e
Wanderson Ricardo <wandersonrpf@gmail.com>
2023-03-14 00:48:51 -0300

    Delete 03-Desenvolvimento-Back-end/Seção06-NodeJS-ORM-e-autenticação/01-ORM-Interface-da-aplicação-com-o-banco-de-dados/Exercise-fixation/src directory

--- 03-Desenvolvimento-Back-end/Seção06-NodeJS-ORM-e-autenticação/01-ORM-Interface-da-aplicação-com-o-banco-de-dados/Exercise-fixation/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const BooksRoter = require('./router/books.router');
-
-const app = express();
-
-app.use(express.json());
-app.use('/books', BooksRoter);
-
-module.exports = app;
--- 03-Desenvolvimento-Back-end/Seção06-NodeJS-ORM-e-autenticação/01-ORM-Interface-da-aplicação-com-o-banco-de-dados/Exercise-fixation/src/router/index.js
+++
@@ -1,5 +0,0 @@
-const BooksRouter = require('../router/books.router');
-
-module.exports = {
-  BooksRouter,
-}
